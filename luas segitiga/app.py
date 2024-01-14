deb414afe0c206b508859952e2d749dad83db134
Ayann12 <tantowi818@gmail.com>
2024-01-14 22:07:29 +0700

    latiihan bosku

---
+++ luas segitiga/app.py
@@ -0,0 +1,7 @@
+panjang = float(input("Masukan panjang persegi panjang : "))
+lebar = float(input("Masukan Lebar : "))
+
+luas = panjang * lebar
+
+
+print("Luas persegi panjang dengan panjang", panjang, "dan lebar", lebar, "adalah", luas)
